fe84463a5584a10a8d499ec65e3af463bd21b15b
Dang Hao <62649837+haotamht@users.noreply.github.com>
2023-05-12 05:28:17 +0700

    cart empty

--- src/pages/AllProduct/index.js
+++ src/pages/AllProduct/index.js
@@ -1,10 +1,48 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown, faChevronUp, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react/headless';
 
 import Product from './Product';
+import DropDown from '~/pages/Product/DropDown';
+import { Link } from 'react-router-dom';
 
 function ProductPage() {
     const [dataPro, setDataPro] = useState({ products: [] });
+    const [hoverList, setHoverList] = useState(false);
+    const [onClickFind, setOnClickFind] = useState(false);
+
+    const contents = [
+        {
+            title: 'TOP',
+            link: '/top',
+            children: [
+                { title: 'T-Shirt', link: '/t-shirt' },
+                { title: 'Shirt & Polo', link: '/shirt&polo' },
+                { title: 'Hoodie & Sweatshirt', link: '/hoodie&sweatshirt' },
+                { title: 'Jacket', link: '/jacket' },
+            ],
+        },
+        {
+            title: 'BOTTOM',
+            link: '/bottom',
+            children: [
+                { title: 'Pants', link: '/pants' },
+                { title: 'Shorts', link: '/shorts' },
+            ],
+        },
+        {
+            title: 'ACCESSORY',
+            link: '/accessory',
+            children: [
+                { title: 'Bag & Backpack', link: '/bag&backpack' },
+                { title: 'Hat', link: '/hat' },
+                { title: 'Others', link: '/others' },
+            ],
+        },
+    ];
+
     const path = 'http://localhost:1209/';
 
     useEffect(() => {
@@ -24,8 +62,37 @@ function ProductPage() {
     }, []);
 
     return (
-        <div>
-            <div className="grid grid-cols-4 gap-y-[50px] px-[50px] mt-[105px]">
+        <div className="px-[50px] mx-[-15px]">
+            <div className="mt-[105px] mb-[15px] px-[15px] flex justify-between items-center leading-[30px] tracking-[0.25px]">
+                <Tippy
+                    interactive
+                    placement="bottom-start"
+                    render={(attrs) => (
+                        <div tabIndex="-1" {...attrs} className="w-[200px] bg-[#fff] opacity-100 text-[12px]">
+                            <Link to="/product/all">
+                                <div>Tất cả</div>
+                            </Link>
+                            {contents.map((content, index) => {
+                                return <DropDown key={index++} content={content} />;
+                            })}
+                        </div>
+                    )}
+                >
+                    <div className="mb-[10px] flex items-center uppercase text-[16px] font-medium">
+                        Danh Mục
+                        <FontAwesomeIcon
+                            icon={hoverList ? faChevronUp : faChevronDown}
+                            className="text-[12px] ml-[5px]"
+                        />
+                    </div>
+                </Tippy>
+
+                <div className="mb-[10px] flex items-center uppercase text-[16px] font-medium">
+                    Tìm Theo <FontAwesomeIcon icon={onClickFind ? faMinus : faPlus} className="text-[12px] ml-[5px]" />
+                </div>
+            </div>
+
+            <div className="grid grid-cols-4 gap-y-[50px]">
                 {dataPro.length !== 0 ? (
                     dataPro.products.map((product) => {
                         return <Product product={product} path={path} />;
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,5 +1,45 @@
+import { Link } from 'react-router-dom';
+
 function CartPage() {
-    return <h1>Cart Page</h1>;
+    return (
+        <div className="pt-[108px] ">
+            <div className="mx-[209px] mt-[30px] mb-[20px] px-[15px]">
+                <div className="mx-[-15px]">
+                    <div className="px-[15px]">
+                        <div className="mb-[10px]">
+                            <div className="mx-[-15px]">
+                                <div className="px-[15px] w-full flex items-center relative">
+                                    <div className="uppercase text-[26px] text-[#1c1c1c] leading-[28px] m-[0] tracking-[2.4px]">
+                                        Giỏ hàng
+                                    </div>
+                                    <div className="ml-[5px] text-[14px] leading-[28px] tracking-[2.4px] absolute top-[10%] left-[10%]">
+                                        (sản phẩm)
+                                        {/* phần này sau có thể thêm đếm số sản phẩm vào */}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="mx-[-15px]">
+                                <div className="max-w-[200px] h-[auto] mx-[auto]">
+                                    <img
+                                        src="//bizweb.dktcdn.net/100/325/189/themes/675912/assets/empty-cart.png?1533693226542"
+                                        alt=""
+                                    />
+                                </div>
+                                <div className="flex items-center justify-center">
+                                    <Link
+                                        to="/"
+                                        className="h-[auto] mt-[20px] px-[30px] py-[15px] text-[#fff] text-[14px] uppercase leading-[1.1] bg-[#1c1c1c] hover:bg-[#dc4e3f] hover:text-[#fff]"
+                                    >
+                                        Tiếp tục lựa chọn
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default CartPage;
