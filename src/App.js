f4d9ca8a587a9ebf717623ca2c693ce36b610fc8
Dang Hao <62649837+haotamht@users.noreply.github.com>
2023-08-06 05:08:17 +0700

    add lazy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import { Fragment, useEffect } from 'react';
+import { Fragment, useEffect, Suspense } from 'react';
 import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { CircularProgress } from '@mui/material';
+import { useDispatch } from 'react-redux';
+import { CircularProgress, Skeleton } from '@mui/material';
 
 import { publicRoutes, privateRoutes, nonDefaultLayoutRoutes } from '~/routes';
 import DefaultLayout from '~/layouts';
@@ -17,7 +17,6 @@ function App() {
         return true ? <Outlet /> : <Navigate to="/login" />;
     };
 
-    const { isLoading } = useSelector((state) => state.productsAll);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -34,53 +33,32 @@ function App() {
             dispatch(setLoading(false));
         }
     }, [dispatch]);
-    if (isLoading) {
-        return (
-            <div className="lg:px-[50px] lg:mx-[-15px]">
-                <div className="flex items-center justify-center">
-                    <CircularProgress color="inherit" />
-                </div>
-            </div>
-        );
-    }
+
     return (
         <div className="text-[#3D3D40] w-full">
-            <Routes>
-                {publicRoutes.map((route, index) => {
-                    const Page = route.component;
-                    let Layout = DefaultLayout;
+            <Suspense
+                fallback={
+                    <div className="lg:px-[50px] lg:mx-[-15px] flex items-center justify-center h-[100vh] w-full">
+                        <div className="flex flex-col items-center justify-center w-[200px]">
+                            <CircularProgress />
+                            <div className="mt-[20px]">Loading ...</div>
+                        </div>
+                    </div>
+                }
+            >
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        console.log(route.component);
+                        let Layout = DefaultLayout;
 
-                    if (route.layout) {
-                        Layout = route.layout;
-                    } else if (route.layout === null) {
-                        Layout = Fragment;
-                    }
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
 
-                    return (
-                        <Route
-                            key={index}
-                            path={route.path}
-                            element={
-                                <Layout>
-                                    <Page />
-                                </Layout>
-                            }
-                        />
-                    );
-                })}
-
-                {privateRoutes.map((route, index) => {
-                    const Page = route.component;
-                    let Layout = DefaultLayout;
-
-                    if (route.layout) {
-                        Layout = route.layout;
-                    } else if (route.layout === null) {
-                        Layout = Fragment;
-                    }
-
-                    return (
-                        <Route key={index} element={<PrivateWrapper />}>
+                        return (
                             <Route
                                 key={index}
                                 path={route.path}
@@ -90,37 +68,62 @@ function App() {
                                     </Layout>
                                 }
                             />
-                        </Route>
-                    );
-                })}
+                        );
+                    })}
 
-                {nonDefaultLayoutRoutes.map((route, index) => {
-                    const Page = route.component;
-                    const Layout = Fragment;
+                    {privateRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
 
-                    return (
-                        <Route key={index} element={<PrivateWrapper />}>
-                            <Route
-                                key={index}
-                                path={route.path}
-                                element={
-                                    <Layout>
-                                        <Page />
-                                    </Layout>
-                                }
-                            />
-                        </Route>
-                    );
-                })}
-                <Route
-                    path="*"
-                    element={
-                        <DefaultLayout>
-                            <NotFound />
-                        </DefaultLayout>
-                    }
-                />
-            </Routes>
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route key={index} element={<PrivateWrapper />}>
+                                <Route
+                                    key={index}
+                                    path={route.path}
+                                    element={
+                                        <Layout>
+                                            <Page />
+                                        </Layout>
+                                    }
+                                />
+                            </Route>
+                        );
+                    })}
+
+                    {nonDefaultLayoutRoutes.map((route, index) => {
+                        const Page = route.component;
+                        const Layout = Fragment;
+
+                        return (
+                            <Route key={index} element={<PrivateWrapper />}>
+                                <Route
+                                    key={index}
+                                    path={route.path}
+                                    element={
+                                        <Layout>
+                                            <Page />
+                                        </Layout>
+                                    }
+                                />
+                            </Route>
+                        );
+                    })}
+                    <Route
+                        path="*"
+                        element={
+                            <DefaultLayout>
+                                <NotFound />
+                            </DefaultLayout>
+                        }
+                    />
+                </Routes>
+            </Suspense>
         </div>
     );
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,42 +1,43 @@
 import config from '~/config';
+import { lazy } from 'react';
 
 //Layouts
 //import { HeaderOnly } from '~/layouts';
 
 //Pages
-import Home from '~/pages/Home';
-import AllProduct from '~/pages/AllProduct';
-import TopPage from '~/pages/Top';
-import Product from '~/pages/Product';
-import Search from '~/pages/Search';
-import ContactPage from '~/pages/Contact';
-import Cart from '~/pages/Cart';
-import Login from '~/pages/Login';
-import Register from '~/pages/Register';
-import Profile from '~/pages/Account/Profile';
-import Orders from '~/pages/Account/Orders';
-import ChangePassword from '~/pages/Account/ChangePassword';
-import Addresses from '~/pages/Account/Addresses';
-import CheckOut from '~/pages/CheckOut';
-import ForgotPassword from '~/pages/ForgotPassword';
-import BottomPage from '~/pages/Bottom';
-import AccessoryPage from '~/pages/Accessory';
-import TShirtPage from '~/pages/TShirt';
-import ShirtPage from '~/pages/Shirt';
-import HoodiePage from '~/pages/Hoodie';
-import JacketPage from '~/pages/Jacket';
-import PantsPage from '~/pages/Pants';
-import ShortsPage from '~/pages/Shorts';
-import BagPage from '~/pages/Bag';
-import HatPage from '~/pages/Hat';
-import OrthersPage from '~/pages/Orthers';
-import AllSale from '~/pages/AllSale';
-import AboutPage from '~/pages/About';
+const Home = lazy(() => import('~/pages/Home'));
+const AllProduct = lazy(() => import('~/pages/AllProduct'));
+const TopPage = lazy(() => import('~/pages/Top'));
+const Product = lazy(() => import('~/pages/Product'));
+const Search = lazy(() => import('~/pages/Search'));
+const ContactPage = lazy(() => import('~/pages/Contact'));
+const Cart = lazy(() => import('~/pages/Cart'));
+const Login = lazy(() => import('~/pages/Login'));
+const Register = lazy(() => import('~/pages/Register'));
+const Profile = lazy(() => import('~/pages/Account/Profile'));
+const Orders = lazy(() => import('~/pages/Account/Orders'));
+const ChangePassword = lazy(() => import('~/pages/Account/ChangePassword'));
+const Addresses = lazy(() => import('~/pages/Account/Addresses'));
+const CheckOut = lazy(() => import('~/pages/CheckOut'));
+const ForgotPassword = lazy(() => import('~/pages/ForgotPassword'));
+const BottomPage = lazy(() => import('~/pages/Bottom'));
+const AccessoryPage = lazy(() => import('~/pages/Accessory'));
+const TShirtPage = lazy(() => import('~/pages/TShirt'));
+const ShirtPage = lazy(() => import('~/pages/Shirt'));
+const HoodiePage = lazy(() => import('~/pages/Hoodie'));
+const JacketPage = lazy(() => import('~/pages/Jacket'));
+const PantsPage = lazy(() => import('~/pages/Pants'));
+const ShortsPage = lazy(() => import('~/pages/Shorts'));
+const BagPage = lazy(() => import('~/pages/Bag'));
+const HatPage = lazy(() => import('~/pages/Hat'));
+const OrthersPage = lazy(() => import('~/pages/Orthers'));
+const AllSale = lazy(() => import('~/pages/AllSale'));
+const AboutPage = lazy(() => import('~/pages/About'));
 //public routes
+
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.allproducts, component: AllProduct },
-
     { path: config.routes.top, component: TopPage },
     { path: config.routes.bottom, component: BottomPage },
     { path: config.routes.accessory, component: AccessoryPage },
